675246c65ff39dad0693f9b65b39fbca90b62355
OTHmanera <raisothmane4@gmail.com>
2023-02-11 22:58:17 +0100

    adding env + route

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv/config');
+const port = process.env.PORT
+const app = express();
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('fs202!')
+})
+
+
+
+app.listen(port,()=>{
+  console.log(`Im working on  ${port}`);
+});
